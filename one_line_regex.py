de92472cfe20c958f95b2bc46b0b1c932a07a689
Sudipta Paul <sudipta116@gmail.com>
2022-04-08 15:06:41 +1000

    One line regex for dead links check

--- one_line_regex.py
+++ one_line_regex.py
@@ -14,3 +14,22 @@ dollars = [x[0] for x in re.findall('(\$[0-9]+(\.[0-9]*)?)', report)]
 ## Result
 print(dollars)
 
+## Data
+article = '''
+The algorithm has important practical applications
+http://blog.finxter.com/applications/
+in many basic data structures such as sets, trees,
+dictionaries, bags, bag trees, bag dictionaries,
+hash sets, https://blog.finxter.com/sets-in-python/
+hash tables, maps, and arrays. http://blog.finxter.com/
+http://not-a-valid-url
+http:/bla.ba.com
+http://bo.bo.bo.bo.bo.bo/
+http://bo.bo.bo.bo.bo.bo/333483--33343-/
+'''
+
+## One-Liner
+stale_links = re.findall('http://[a-z0-9_\-.]+\.[a-z0-9_\-/]+', article)
+
+## Results
+print(stale_links)
